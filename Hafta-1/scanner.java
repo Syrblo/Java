728406ab0357d799ed134ea272faee83d84dfba0
Ahmet Fatih Arslan <105457581+Syrblo@users.noreply.github.com>
2026-02-11 17:34:11 +0300

    Update scanner.java

--- Hafta-1/scanner.java
+++ Hafta-1/scanner.java
@@ -7,9 +7,12 @@ public class scanner {
         int sayi1, sayi2, toplam;
         System.out.print("Lutfen sayi1 giriniz: ");
         sayi1 = giris.nextInt();
+        
         System.out.print("Lutfen sayi2 giriniz: ");
         sayi2 = giris.nextInt();
+        
         toplam = sayi1+sayi2;
         System.out.println("Sayilarin toplami: " + toplam);
     }
+
 }
