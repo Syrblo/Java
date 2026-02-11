72c6217644e1edfe836192155925f3384868cdda
Ahmet Fatih Arslan <105457581+Syrblo@users.noreply.github.com>
2026-02-11 17:33:50 +0300

    Add files via upload

---
+++ Hafta-1/scanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class scanner {
+    public static void  main(String[] args){
+        Scanner giris=new Scanner(System.in);
+        
+        int sayi1, sayi2, toplam;
+        System.out.print("Lutfen sayi1 giriniz: ");
+        sayi1 = giris.nextInt();
+        System.out.print("Lutfen sayi2 giriniz: ");
+        sayi2 = giris.nextInt();
+        toplam = sayi1+sayi2;
+        System.out.println("Sayilarin toplami: " + toplam);
+    }
+}
